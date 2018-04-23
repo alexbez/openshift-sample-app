1bcb0d25b520a4d6a7511e80d6f31ac3f5ac6035
HCP Demo User <hybridclouddemo@gmail.com>
2018-04-23 09:08:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+OpenShift Sample Application
+============================
+
+This is the sample two-tier application (java front end + postgresql database backend) for OpenShift
+
---
+++ src/main/java/com/openshift/hcp/DBAccountNumberGenerator.java
@@ -0,0 +1,50 @@
+package com.openshift.hcp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+
+public class DBAccountNumberGenerator extends AccountNumberGenerator {
+
+	private static final long serialVersionUID = 4134149485208505751L;
+	private static final Logger LOGGER = Logger.getLogger( "DBAccountNumberGenerator" );
+	@Override
+	public String[] generateAccountNumbers() {
+	int num_acc = 0;
+	LOGGER.log(Level.INFO, "Entering generateAccountNumbers()...");
+	ArrayList<String> list = new ArrayList<String>();
+	try {
+			String databaseURL = "jdbc:postgresql://";
+			databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
+			databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
+			String username = System.getenv("POSTGRESQL_USER");
+			String password = System.getenv("PGPASSWORD");
+			Class.forName("org.postgresql.Driver"); 			
+			Connection connection = DriverManager.getConnection(databaseURL, username, password);
+			if (connection != null) {
+				LOGGER.log(level.INFO, "Loading account numbers from the database");
+				String SQL = "select a.ac_number as account_number from account a";
+				Statement stmt = connection.createStatement();
+				ResultSet rs = stmt.executeQuery(SQL);
+				while (rs.next()) {
+					list.add(rs.getString("account_number"));
+					num_acc++;
+				}
+				LOGGER.log("Number of records read: " + num_acc);
+				rs.close();
+				connection.close();
+			} else
+				LOGGER.log(Level.SEVERE, "Could not connect to the database.");
+	} catch (Exception e) {
+		LOGGER.log(Level.SEVERE, e.toString(), e);
+		System.out.println("Database connection problem!");
+	}
+	return list.toArray(new String[list.size()]);	  
+}
+
+}
